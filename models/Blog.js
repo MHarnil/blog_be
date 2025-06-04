24eca825d7c20c73a78f2f25a920326b08ca770c
harnil <mharnil@gmail.com>
2025-06-04 11:15:15 +0530

    first commit.

--- models/Blog.js
+++ models/Blog.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 const blogSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String },
-    image: { type: String }
+    image: { type: String },
+    type: { type: String, required: true },
 }, { timestamps: true });
 
 export default mongoose.model('Blog', blogSchema);
